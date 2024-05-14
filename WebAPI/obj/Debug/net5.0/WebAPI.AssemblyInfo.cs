e29dd4b9603289033001a36b374c08b83f3ce6dd
hoaithanhvo <115620157+hoaithanhvo@users.noreply.github.com>
2024-05-14 22:17:22 +0700

    fix conflic

--- WebAPI/obj/Debug/net5.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net5.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a888fdef975721ba3d9c68a26b977a70d51854e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
